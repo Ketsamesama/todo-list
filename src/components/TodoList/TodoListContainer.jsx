65329b7ad67e55e2f2dd977882f7975d26d3c9a6
Vlad <vladslantsitskiy@gmail.com>
2022-06-13 21:27:14 +0300

    refactiring

--- src/components/TodoList/TodoListContainer.jsx
+++ src/components/TodoList/TodoListContainer.jsx
@@ -9,22 +9,7 @@ import {
 import TodoList from './TodoList';
 import { useLocation } from 'react-router-dom';
 
-
-function TodoListContainer() {
-  const dispatch = useDispatch();
-  let url = useLocation().pathname;
-
-  useEffect(() => {
-    dispatch(getTodoInLocal());
-    let data = `${new Date().getDate()} ${
-      new Date().getMonth() + 1
-    } ${new Date().getFullYear()}`;
-    dispatch(setData(data));
-  }, []);
-
-  let todos = useSelector((state) => state.todo.todos),
-    data = useSelector((state) => state.todo.data);
-
+function getActualTodoList(url, todos) {
   switch (url) {
     case '/active':
       if (todos.length > 0) {
@@ -42,9 +27,25 @@ function TodoListContainer() {
     default:
       break;
   }
+  return todos;
+}
 
-  console.log(todos);
+function TodoListContainer() {
+  const dispatch = useDispatch();
+  let url = useLocation().pathname;
 
+  useEffect(() => {
+    dispatch(getTodoInLocal());
+    let data = `${new Date().getDate()} ${
+      new Date().getMonth() + 1
+    } ${new Date().getFullYear()}`;
+    dispatch(setData(data));
+  }, []);
+
+  let todos = useSelector((state) => state.todo.todos),
+    data = useSelector((state) => state.todo.data);
+
+  todos = getActualTodoList(url, todos);
   return (
     <TodoList
       todos={todos}
